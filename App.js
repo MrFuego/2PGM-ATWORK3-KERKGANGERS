5758b592967838b6ce6c1106c0de9fc8f696df86
alecmeganck99 <alecmeganck@icloud.com>
2020-12-27 16:06:39 +0100

    upload navigation

--- App.js
+++ App.js
@@ -1,21 +1,11 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import Welcomescreen from './app/screens/Welcomescreen';
-import colors from './app/config/colors'
+import { NavigationContainer } from '@react-navigation/native';
+import AuthNavigator from './app/navigation/AuthNavigator';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Welcomescreen />
-    </View>
+    <NavigationContainer>
+      <AuthNavigator />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: colors.grey,
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  }
-})
---
+++ app/utils/AuthNavigator.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Startscreen, Welcomescreen } from '../screens';
+
+const Stack = createStackNavigator();
+
+const AuthNavigator = () => (
+  <Stack.Navigator screenOptions={{ headerShown: false }}>
+    <Stack.Screen name="Welcome" component={Welcomescreen} />
+    <Stack.Screen name="Start" component={Startscreen} />
+  </Stack.Navigator>
+)
+
+export default AuthNavigator;
