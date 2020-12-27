28af3a2ce056cd9a771d988b7e1380baa42fb5fc
alecmeganck99 <alecmeganck@icloud.com>
2020-12-27 11:51:52 +0100

    add welcomescreen

--- App.js
+++ App.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
+import Welcomescreen from './app/screens/Welcomescreen';
+import colors from './app/config/colors'
 
 export default function App() {
   return (
     <View style={styles.container}>
-
+      <Welcomescreen />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: colors.grey,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  }
+})
