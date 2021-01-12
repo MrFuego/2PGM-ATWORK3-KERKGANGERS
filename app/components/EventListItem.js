cccc64eee840a3b4a5f8c20b5b7e6e48d0df3344
Louis Martens <louimart3@student.arteveldehs.be>
2021-01-12 20:11:48 +0100

    meer lezen toegevoegd

--- app/components/EventListItem.js
+++ app/components/EventListItem.js
@@ -25,12 +25,15 @@ export default function EventListItem() {
             data={data}
             keyExtractor={({ id }) => id}
             renderItem={({ item }) => (
-              <TouchableOpacity style={styles.card} onPress={() => Linking.openURL("https://mkgent.be/activiteiten/")}>
+              <TouchableOpacity style={styles.card} onPress={() => Linking.openURL(item.fields.readMore)} >
                   <Text style={styles.event}>
                     {item.fields.eventName}
                   </Text>
                   <Text style={styles.date}>
                     {item.fields.date}
+                  </Text>
+                  <Text style={styles.more}>
+                    meer info
                   </Text>
               </TouchableOpacity>
             )}
@@ -50,6 +53,7 @@ const styles = StyleSheet.create({
     borderColor: colors.gold,
     borderWidth: 2,
   },
+
   event: {
     color: colors.white,
     fontSize: 20,
@@ -58,7 +62,15 @@ const styles = StyleSheet.create({
     textDecorationLine: "underline",
     textDecorationStyle: "solid",
   },
+
   date: {
     color: colors.gold,
+  },
+
+  more: {
+    fontWeight: 'bold',
+    color: "#fff",
+    marginTop: 7,
+    fontSize: 17,
   }
 })
