c85a9131433d799d89ab8a2d0339a9ee4cc13314
Louis Martens <louimart3@student.arteveldehs.be>
2021-01-19 16:10:31 +0100

    rules added

--- app/screens/Infoscreen.js
+++ app/screens/Infoscreen.js
@@ -23,11 +23,7 @@ export default function Infoscreen({ navigation, route }) {
           <AppText style={styles.title}>Regels</AppText>
           <View style={styles.block}>
             <AppText style={styles.subTitle}>Regel 1</AppText>
-            <AppText style={styles.desc}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nibh felis, auctor et tempus eu, luctus eget lacus. Nam egestas nibh vitae lorem maximus, vel tempus nibh ultricies. Vivamus hendrerit ut nunc id vehicula. Nam fringilla tortor eget nisi luctus, eget faucibus tellus cursus. Pellentesque nulla erat, mattis ac arcu id, aliquam elementum augue. Proin semper eu quam in accumsan. Sed et nisl venenatis, aliquam eros et, rhoncus nulla.</AppText>
-          </View>
-          <View style={styles.block}>
-            <AppText style={styles.subTitle}>Regel 2</AppText>
-            <AppText style={styles.desc}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nibh felis, auctor et tempus eu, luctus eget lacus. Nam egestas nibh vitae lorem maximus, vel tempus nibh ultricies. Vivamus hendrerit ut nunc id vehicula. Nam fringilla tortor eget nisi luctus, eget faucibus tellus cursus. Pellentesque nulla erat, mattis ac arcu id, aliquam elementum augue. Proin semper eu quam in accumsan. Sed et nisl venenatis, aliquam eros et, rhoncus nulla.</AppText>
+            <AppText style={styles.desc}>{params.fields.rules}</AppText>
           </View>
         </View>
       </ScrollView>
